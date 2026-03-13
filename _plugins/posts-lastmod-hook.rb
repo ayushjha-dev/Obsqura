8ea50f6482aa5bf1cf290bbb98a926aacedbb2d9
Ayush Kumar Jha <sanataniayushjha07@gmail.com>
2026-03-13 13:05:07 +0530

    Refactor git command execution in lastmod-hook
    Refactor to use Open3 for capturing git command output and handling errors.

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,13 +2,25 @@
 #
 # Check for changed posts
 
+require 'open3'
+
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  stdout, stderr, status = Open3.capture3('git', 'rev-list', '--count', 'HEAD', post.path.to_s)
+  unless status.success?
+    Jekyll.logger.warn('lastmod-hook:', "git rev-list failed for #{post.path}: #{stderr.strip}")
+    next
+  end
+
+  commit_num = stdout.strip
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    lastmod_stdout, lastmod_stderr, lastmod_status = Open3.capture3('git', 'log', '-1', '--pretty=%ad', '--date=iso', post.path.to_s)
+    if lastmod_status.success?
+      post.data['last_modified_at'] = lastmod_stdout.strip
+    else
+      Jekyll.logger.warn('lastmod-hook:', "git log failed for #{post.path}: #{lastmod_stderr.strip}")
+    end
   end
 
 end
